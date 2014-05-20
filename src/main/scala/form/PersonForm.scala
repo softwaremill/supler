c0406a1c68f11357c052634449827ffeb60ae216
adamw <adam@warski.org>
2014-05-20 21:43:17 +0200

    Alternative DSL - proposition

--- src/main/scala/form/PersonForm.scala
+++ src/main/scala/form/PersonForm.scala
@@ -4,9 +4,12 @@ import entity.Person
 import DSL._
 
 object PersonForm {
-  val personForm = form[Person](
-    row(field(_.name, length(10), validation((entity, value: String) => value != null), dataProvider(_ => List("a")))),
-    row(field(_.lastName), field(_.lastName)),
-    row(field(_.shoeNumber))
-  )
+  val takieSobiePoleOdCzapy = field[Person, Int](_.shoeNumber).validate(custom((e, v) => v == 10))
+
+  val personForm = form[Person](f => List(
+    f.field(_.name).validate(length(10), custom((entity, value) => value != null)).use(dataProvider(_ => List("a"))),
+    f.row(f.field(_.lastName), f.field(_.lastName)),
+    f.field(_.shoeNumber),
+    takieSobiePoleOdCzapy
+  ))
 }
