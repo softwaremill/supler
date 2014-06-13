abc1f7023fd7d0452c44f6ec7717dbd5fda6d7ec
szimano <szimano@szimano.org>
2014-06-13 17:06:38 +0200

    allow only _.fieldName field references

--- supler/src/main/scala/form/Supler.scala
+++ supler/src/main/scala/form/Supler.scala
@@ -9,9 +9,9 @@ object Supler extends Validators {
     rows(new Supler[T])
   }
 
-  def newField[T, U](fieldName: String): Field[T, U]  = {
+  def newField[T, U](fieldName: String): Field[T, U] = {
     println(s"Running field $fieldName")
-    Field[T,U](fieldName, List(), None)
+    Field[T, U](fieldName, List(), None)
   }
 
   def dataProvider[T, U](provider: T => List[U]): DataProvider[T, U] = {
@@ -19,39 +19,52 @@ object Supler extends Validators {
     new DataProvider[T, U]
   }
 
-  def field[T, U](param: T => U): Field[T,U] = macro Supler.field_impl[T, U]
+  def field[T, U](param: T => U): Field[T, U] = macro Supler.field_impl[T, U]
 
   def field_impl[T, U](c: Context)(param: c.Expr[T => U]): c.Expr[Field[T, U]] = {
     import c.universe._
-    val paramRep = show(param.tree)
-    val paramRepTree = Literal(Constant(paramRep))
+    println(showRaw(param))
+
+    val fieldName = param match {
+      case Expr(
+      Function(
+      List(ValDef(Modifiers(_), TermName(termDef: String), TypeTree(), EmptyTree)),
+      Select(Ident(TermName(termUse: String)), TermName(field: String)))) =>
+        if (termDef == termUse) {
+          field
+        }
+      case _ => throw new IllegalArgumentException("Illegal field reference " + show(param.tree) + "; please use _.fieldName instead")
+    }
+
+    val paramRepTree = Literal(Constant(fieldName))
     val paramRepExpr = c.Expr[String](paramRepTree)
 
     // TODO get the proper field name from inside
     reify {
-      println (paramRepExpr.splice)
+      println(paramRepExpr.splice)
       newField(paramRepExpr.splice)
     }
   }
 }
 
 class Supler[T] extends Validators {
-  def field[U](param: T => U): Field[T,U] = macro Supler.field_impl[T, U]
+  def field[U](param: T => U): Field[T, U] = macro Supler.field_impl[T, U]
 }
 
 class Row[T] {
-  def || (field: Field[T, _]): Row[T] = {
-    println (s"new row with field $field")
+  def ||(field: Field[T, _]): Row[T] = {
+    println(s"new row with field $field")
     this
   }
 }
 
 case class Field[T, U](
-  name: String,
-  validators: List[Validator[T, U]],
-  dataProvider: Option[DataProvider[T, U]]) extends Row[T] {
+                        name: String,
+                        validators: List[Validator[T, U]],
+                        dataProvider: Option[DataProvider[T, U]]) extends Row[T] {
 
   def validate(validators: Validator[T, U]*): Field[T, U] = this.copy(validators = this.validators ++ validators)
+
   def use(dataProvider: DataProvider[T, U]): Field[T, U] = this.dataProvider match {
     case Some(_) => throw new IllegalStateException("A data provider is already defined!")
     case None => this.copy(dataProvider = Some(dataProvider))
