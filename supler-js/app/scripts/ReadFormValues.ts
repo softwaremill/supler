9b60b5e94efe0bf0f49cc6dfaddfe4039a0cbcec
adamw <adam@warski.org>
2014-08-29 17:39:54 +0200

    Extracting class to read form values

---
+++ supler-js/app/scripts/ReadFormValues.ts
@@ -0,0 +1,58 @@
+class ReadFormValues {
+    getValueFrom(element) {
+        var children = element.childNodes;
+        var result = {};
+
+        for (var i=0; i<children.length; i++) {
+            if (ReadFormValues.isElement(children[i])) {
+                var fieldType = children[i].getAttribute("supler:fieldType");
+                var multiple = children[i].getAttribute("supler:multiple") === "true";
+                if (fieldType) {
+                    var fieldName = children[i].getAttribute("name");
+                    switch (fieldType) {
+                        case "string":
+                            ReadFormValues.appendFieldValue(result, fieldName, children[i].value, multiple);
+                            break;
+
+                        case "integer":
+                            ReadFormValues.appendFieldValue(result, fieldName, parseInt(children[i].value), multiple);
+                            break;
+
+                        case "subform":
+                            // the name is on the fieldset, which is the parent, not on the per-form container.
+                            fieldName = children[i].getAttribute("supler:fieldName");
+                            var subResult = this.getValueFrom(children[i]);
+                            ReadFormValues.appendFieldValue(result, fieldName, subResult, multiple);
+                            break;
+                    }
+                } else if (children[i].childNodes.length > 0) {
+                    // flattening
+                    var childResult = this.getValueFrom(children[i]);
+                    Util.foreach(childResult, (field, v) => {
+                        ReadFormValues.appendFieldValue(result, field, v, multiple);
+                    });
+                }
+            }
+        }
+
+        return result;
+    }
+
+    // http://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
+    private static isElement(o) {
+        return (typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
+            o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName === "string");
+    }
+
+    private static appendFieldValue(result, fieldName, fieldValue, multiple) {
+        if (result[fieldName] && multiple) {
+            result[fieldName].push(fieldValue);
+        } else {
+            if (multiple) {
+                result[fieldName] = [ fieldValue ];
+            } else {
+                result[fieldName] = fieldValue;
+            }
+        }
+}
+}
--- supler-js/app/scripts/supler.ts
+++ supler-js/app/scripts/supler.ts
@@ -78,66 +78,7 @@ class SuplerForm {
     }
 
     getValue() {
-        return this.getValueFrom(this.container);
-    }
-
-    getValueFrom(element) {
-        var children = element.childNodes;
-        var result = {};
-
-        function appendFieldValue(fieldName, fieldValue, multiple) {
-            if (result[fieldName] && multiple) {
-                result[fieldName].push(fieldValue);
-            } else {
-                if (multiple) {
-                    result[fieldName] = [ fieldValue ];
-                } else {
-                    result[fieldName] = fieldValue;
-                }
-            }
-        }
-
-        // http://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
-        function isElement(o){
-            return (
-                    typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
-                o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string"
-                );
-        }
-
-        for (var i=0; i<children.length; i++) {
-            if (isElement(children[i])) {
-                var fieldType = children[i].getAttribute("supler:fieldType");
-                var multiple = children[i].getAttribute("supler:multiple") === "true";
-                if (fieldType) {
-                    var fieldName = children[i].getAttribute("name");
-                    switch (fieldType) {
-                        case "string":
-                            appendFieldValue(fieldName, children[i].value, multiple);
-                            break;
-
-                        case "integer":
-                            appendFieldValue(fieldName, parseInt(children[i].value), multiple);
-                            break;
-
-                        case "subform":
-                            // the name is on the fieldset, which is the parent, not on the per-form container.
-                            fieldName = children[i].getAttribute("supler:fieldName");
-                            var subResult = this.getValueFrom(children[i]);
-                            appendFieldValue(fieldName, subResult, multiple);
-                            break;
-                    }
-                } else if (children[i].childNodes.length > 0) {
-                    // flattening
-                    var childResult = this.getValueFrom(children[i]);
-                    Util.foreach(childResult, (field, v) => {
-                        appendFieldValue(field, v, multiple);
-                    });
-                }
-            }
-        }
-
-        return result;
+        return new ReadFormValues().getValueFrom(this.container);
     }
 
     nextId() {
