369f13c7f547ebbc2f2a0141d4236beae7c54de3
adamw <adam@warski.org>
2014-08-29 17:54:48 +0200

    Static fns look ugly ;)

--- supler-js/app/scripts/ReadFormValues.ts
+++ supler-js/app/scripts/ReadFormValues.ts
@@ -4,32 +4,32 @@ class ReadFormValues {
         var result = {};
 
         for (var i=0; i<children.length; i++) {
-            if (ReadFormValues.isElement(children[i])) {
+            if (this.isElement(children[i])) {
                 var fieldType = children[i].getAttribute("supler:fieldType");
                 var multiple = children[i].getAttribute("supler:multiple") === "true";
                 if (fieldType) {
                     var fieldName = children[i].getAttribute("name");
                     switch (fieldType) {
                         case "string":
-                            ReadFormValues.appendFieldValue(result, fieldName, children[i].value, multiple);
+                            this.appendFieldValue(result, fieldName, children[i].value, multiple);
                             break;
 
                         case "integer":
-                            ReadFormValues.appendFieldValue(result, fieldName, parseInt(children[i].value), multiple);
+                            this.appendFieldValue(result, fieldName, parseInt(children[i].value), multiple);
                             break;
 
                         case "subform":
                             // the name is on the fieldset, which is the parent, not on the per-form container.
                             fieldName = children[i].getAttribute("supler:fieldName");
                             var subResult = this.getValueFrom(children[i]);
-                            ReadFormValues.appendFieldValue(result, fieldName, subResult, multiple);
+                            this.appendFieldValue(result, fieldName, subResult, multiple);
                             break;
                     }
                 } else if (children[i].childNodes.length > 0) {
                     // flattening
                     var childResult = this.getValueFrom(children[i]);
                     Util.foreach(childResult, (field, v) => {
-                        ReadFormValues.appendFieldValue(result, field, v, multiple);
+                        this.appendFieldValue(result, field, v, multiple);
                     });
                 }
             }
@@ -39,12 +39,12 @@ class ReadFormValues {
     }
 
     // http://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
-    private static isElement(o) {
+    private isElement(o) {
         return (typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
             o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName === "string");
     }
 
-    private static appendFieldValue(result, fieldName, fieldValue, multiple) {
+    private appendFieldValue(result, fieldName, fieldValue, multiple) {
         if (result[fieldName] && multiple) {
             result[fieldName].push(fieldValue);
         } else {
@@ -54,5 +54,5 @@ class ReadFormValues {
                 result[fieldName] = fieldValue;
             }
         }
-}
+    }
 }
