524551a0983f1769bc2baa4cf3f107dc935fe42d
adamw <adam@warski.org>
2014-09-28 22:42:37 -0700

    [minor] quotes

--- supler-js/app/scripts/Constants.ts
+++ supler-js/app/scripts/Constants.ts
@@ -1,13 +1,13 @@
 class FieldTypes {
-    static STRING = "string";
-    static INTEGER = "integer";
-    static SELECT = "select";
-    static SUBFORM = "subform"
+    static STRING = 'string';
+    static INTEGER = 'integer';
+    static SELECT = 'select';
+    static SUBFORM = 'subform';
 }
 
 class SuplerAttributes {
-    static FIELD_TYPE = "supler:fieldType";
-    static MULTIPLE = "supler:multiple";
-    static FIELD_NAME = "supler:fieldName";
-    static VALIDATION_ID = "supler:validationId";
+    static FIELD_TYPE = 'supler:fieldType';
+    static MULTIPLE = 'supler:multiple';
+    static FIELD_NAME = 'supler:fieldName';
+    static VALIDATION_ID = 'supler:validationId';
 }
