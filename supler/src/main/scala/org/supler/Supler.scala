f63c5ef42918191174456974c442e443ff0aee70
adamw <adam@warski.org>
2014-11-26 12:26:18 +0100

    Removing unused

--- supler/src/main/scala/org/supler/Supler.scala
+++ supler/src/main/scala/org/supler/Supler.scala
@@ -1,12 +1,10 @@
 package org.supler
 
-import java.util.concurrent.atomic.AtomicLong
-
 import org.json4s.JsonAST.JField
 import org.json4s._
+import org.supler.errors._
 import org.supler.field._
 import org.supler.transformation.FullTransformer
-import org.supler.errors._
 
 import scala.language.experimental.macros
 
@@ -82,9 +80,4 @@ case class MultiFieldRow[T](fields: List[Field[T, _]]) extends Row[T] {
     Row.applyJSONValues(fields, parentPath, obj, jsonFields)
 
   override def generateJSON(obj: T) = fields.flatMap(_.generateJSON(obj))
-}
-
-object IdGenerator {
-  private val counter = new AtomicLong(0)
-  def nextId() = "ID" + counter.getAndIncrement
 }
