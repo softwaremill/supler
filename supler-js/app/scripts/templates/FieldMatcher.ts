d44fe7d039ea0781afa7290563a8a24cb09d70f1
adamw <adam@warski.org>
2014-12-03 14:36:41 +0100

    [refactor] splitting into smaller files

---
+++ supler-js/app/scripts/templates/FieldMatcher.ts
@@ -0,0 +1,55 @@
+interface FieldMatcher {
+    matches(path: string, type: string, renderHintName: string): boolean
+}
+
+class AllFieldMatcher implements FieldMatcher {
+    matches(path: string, type: string, renderHintName: string): boolean { return true; }
+}
+
+class CompositeFieldMatcher implements FieldMatcher {
+    constructor(private m1: FieldMatcher, private m2: FieldMatcher) {}
+    matches(path: string, type: string, renderHintName: string): boolean {
+        return this.m1.matches(path, type, renderHintName) && this.m2.matches(path, type, renderHintName);
+    }
+}
+
+class PathFieldMatcher implements FieldMatcher {
+    constructor(private path: string) {}
+    matches(path: string, type: string, renderHintName: string): boolean { return this.path === path; }
+}
+
+class TypeFieldMatcher implements FieldMatcher {
+    constructor(private type: string) {}
+    matches(path: string, type: string, renderHintName: string): boolean { return this.type === type; }
+}
+
+class RenderHintFieldMatcher implements FieldMatcher {
+    constructor(private renderHintName: string) {}
+    matches(path: string, type: string, renderHintName: string): boolean { return this.renderHintName === renderHintName; }
+}
+
+class FieldMatcherParser {
+    private static FIELD_PATH_MATCHER = 'supler:fieldPath';
+    private static FIELD_TYPE_MATCHER = 'supler:fieldType';
+    private static FIELD_RENDERHINT_MATCHER = 'supler:fieldRenderHint';
+
+    /**
+     * Extracts a field matcher which will determine if a template is applicable for a particular field.
+     */
+    static parseMatcher(element: HTMLElement): FieldMatcher {
+        var current = new AllFieldMatcher();
+        if (element.hasAttribute(FieldMatcherParser.FIELD_PATH_MATCHER)) {
+            current = new CompositeFieldMatcher(current,
+                new PathFieldMatcher(element.getAttribute(FieldMatcherParser.FIELD_PATH_MATCHER)))
+        }
+        if (element.hasAttribute(FieldMatcherParser.FIELD_TYPE_MATCHER)) {
+            current = new CompositeFieldMatcher(current,
+                new TypeFieldMatcher(element.getAttribute(FieldMatcherParser.FIELD_TYPE_MATCHER)))
+        }
+        if (element.hasAttribute(FieldMatcherParser.FIELD_RENDERHINT_MATCHER)) {
+            current = new CompositeFieldMatcher(current,
+                new RenderHintFieldMatcher(element.getAttribute(FieldMatcherParser.FIELD_RENDERHINT_MATCHER)))
+        }
+        return current;
+    }
+}
