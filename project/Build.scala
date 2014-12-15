3d66a716d4529dde85d6c090e43b522a87122310
adamw <adam@warski.org>
2014-12-15 11:19:35 +0100

    Updating scala

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object BuildSettings {
   val buildSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "com.softwaremill",
     version := "1-SNAPSHOT",
-    scalaVersion := "2.11.2",
+    scalaVersion := "2.11.4",
     scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-language:existentials", "-language:higherKinds"),
     licenses := ("Apache2", new java.net.URL("http://www.apache.org/licenses/LICENSE-2.0.txt")) :: Nil
   )
