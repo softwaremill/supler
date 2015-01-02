95c147252277b36c87b20336fed7a8c6d7c1f3f9
adamw <adam@warski.org>
2015-01-02 10:43:51 +0100

    Only adding validation text if it isn't already added

--- supler-js/src/validation/ValidatorRenderOptions.ts
+++ supler-js/src/validation/ValidatorRenderOptions.ts
@@ -1,13 +1,16 @@
 class ValidatorRenderOptions {
   appendValidation(text:string, validationElement:HTMLElement, formElement:HTMLElement) {
     var current = validationElement.innerHTML;
-    if (current && current.length > 0) {
-      validationElement.innerHTML = current + '; ' + text;
-    } else {
-      validationElement.innerHTML = text;
-    }
 
-    HtmlUtil.addClass(formElement.parentElement, 'has-error');
+    if (current.indexOf(text) === -1) {
+      if (current && current.length > 0) {
+        validationElement.innerHTML = current + '; ' + text;
+      } else {
+        validationElement.innerHTML = text;
+      }
+
+      HtmlUtil.addClass(formElement.parentElement, 'has-error');
+    }
   }
 
   removeValidation(validationElement:HTMLElement, formElement:HTMLElement) {
