3051d13a975bb9753ad7baec25544cd2e392267c
szimano <szimano@szimano.org>
2015-02-19 09:20:54 +0200

    script for checking that the newest version of compiles supler.js and supler.min.js have been commited /fix bash return/

--- .check.supler.commited.sh
+++ .check.supler.commited.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ `git status --porcelain supler.js` != "" ]] || [[ `git status --porcelain supler.min.js` != "" ]]
 then
-    return -100
+    exit 100
 else
-    return 0
+    exit 0
 fi
