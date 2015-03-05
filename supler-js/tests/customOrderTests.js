98e42b27b6a2d34b0928f927318a54223292ed4d
szimano <szimano@szimano.org>
2015-03-05 14:28:01 +0200

    #44 - tests for field generation

--- supler-js/tests/customOrderTests.js
+++ supler-js/tests/customOrderTests.js
@@ -12,4 +12,21 @@ describe('customOrder', function(){
     var shownFields = suplerFields();
     shownFields.should.have.lengthOf(2);
   });
+
+  it('should render fields in rows', function() {
+    // given
+    var sf = new Supler.Form(container);
+
+    // when
+    sf.render(data.simple1rows.form1rows);
+
+    // then
+    var field1Parent = byName('field1').parents("div.row")[0];
+    var field2Parent = byName('field2').parents("div.row")[0];
+    var field3Parent = byName('field3').parents("div.row")[0];
+    var field4Parent = byName('field4').parents("div.row")[0];
+    field1Parent.should.equal(field2Parent);
+    field3Parent.should.equal(field4Parent);
+    field1Parent.should.not.equal(field4Parent);
+  });
 });
